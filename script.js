064f6d650ec221edbd8cec157891592e54bef0d0
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2023-10-16 18:54:11 -0300

    atualizando todo o site

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('.conteudo-principal', {duration:2000})
+sr.reveal('.main', {duration:3000})
+sr.reveal('.text-cardapio', {duration:1000})
+sr.reveal('.itens-cardapio', {duration:1000})
+sr.reveal('.contatos', {duration:3000})
+
+
+window.addEventListener('scroll', function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 50)
+})
+
 let show = true;
-const menuContent = document.querySelector('.content');
+const menuContent = document.querySelector('#header');
 const menuToggle = menuContent.querySelector('.menu-toggle');
 
 
