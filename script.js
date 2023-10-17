5b312417029fe322242f9a54f7d25821d64e4959
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2023-10-17 10:34:28 -0300

    update script

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let show = true;
 const menuContent = document.querySelector('#header');
 const menuToggle = menuContent.querySelector('.menu-toggle');
 
-
 menuToggle.addEventListener('click', () => {
     menuContent.classList.toggle('on',show);
     show = !show;
